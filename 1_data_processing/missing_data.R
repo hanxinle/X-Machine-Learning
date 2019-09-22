923d653421a7303be0c808c2c4dc62b6369c3ab0
hanxinle <hanxinlebaby@qq.com>
2019-09-22 23:45:40 +0800

    del R files

--- 1_data_processing/missing_data.R
+++
@@ -1,8 +0,0 @@
-# Data Preprocessing
-
-# Importing the dataset
-dataset = read.csv('Data.csv')
-
-# Taking care of missing data
-dataset$Age[is.na(dataset$Age)] = mean(dataset$Age, na.rm = T)
-dataset$Salary[is.na(dataset$Salary)] = mean(dataset$Salary, na.rm = T)
