1e9f49b13b850bd04b3bba9640aa72f04d9500c1
Han Levipo <hanxloop@foxmail.com>
2019-10-04 06:30:32 +0800

    update to chinese

--- 1_data_processing/data_processing_application.py
+++ 1_data_processing/data_processing_application.py
@@ -31,7 +31,7 @@ y = labelencoder_y.fit_transform(y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# 特征缩放
 
 #from sklearn.preprocessing import StandardScaler
 #sc_X = StandardScaler()
